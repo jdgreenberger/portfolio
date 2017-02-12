905f1ebac3bfb3a14a1d667326ebe130846fe449
Josh Greenberger <jdgreenberger@gmail.com>
2017-02-12 09:28:17 -0800

    updates

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-{}
+var active = false
+
+function addPopupActive () {
+  active = !active
+  if (active) {
+    document.getElementById('popup').className += '.active'
+  } else {
+    console.log(document.getElementById('popup').className)
+    document.getElementById('popup').className = 'popup'
+  }
+}
